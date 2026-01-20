90357f63b56b99f93095ae05103434ad15481a32
Durgesh <ds6636297@gmail.com>
2026-01-21 00:22:14 +0530

    added visualisation for dijkstra

--- app/dijkstra/simulation/page.tsx
+++ app/dijkstra/simulation/page.tsx
@@ -1,12 +1,13 @@
 import AlgorithmLayout from '@/components/AlgorithmLayout'
+import DijkstraVisualizer from '@/components/visualizers/DijkstraVisualizer'
+
+
 
 export default function DijkstraSimulationPage() {
   return (
     <AlgorithmLayout title="Dijkstra's Algorithm" algorithm="dijkstra">
-      <section className="py-12">
-        <div className="border border-gray-300 bg-white rounded-lg p-8 min-h-[600px]">
-          {/* Placeholder - visualization will be added later */}
-        </div>
+      <section className="py-8">
+        <DijkstraVisualizer />
       </section>
     </AlgorithmLayout>
   )
